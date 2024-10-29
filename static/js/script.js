40f64fc505f7deb7efde0a86e356899cbc434b88
Rayagz77 <nadegekiemde.cdawch@gmail.com>
2024-10-29 22:27:29 +0100

    Header and lateral navbar added

---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+function showPopup(title, description, price, image) {
+    const popup = document.createElement('div');
+    popup.className = 'popup';
+    popup.innerHTML = `
+        <div class="popup-content">
+            <span class="close" onclick="this.parentElement.parentElement.remove();">&times;</span>
+            <img src="${image}" alt="${title}">
+            <h2>${title}</h2>
+            <p>Prix : ${price} €</p>
+            <p>${description}</p>
+        </div>
+    `;
+    document.body.appendChild(popup);
+}
+
+document.getElementById("price-range").addEventListener("input", function() {
+    document.getElementById("price-value").textContent = this.value;
+});
